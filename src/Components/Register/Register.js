b4b71352a715a6a585c99c9d40eb5ebc08dd8664
hasibulhasan44 <hasanhasib7367@gmail.com>
2022-10-25 20:47:22 +0600

    register form almost done

--- src/Components/Register/Register.js
+++ src/Components/Register/Register.js
@@ -3,7 +3,15 @@ import React from "react";
 const Register = () => {
   return (
     <div className="w-1/4 mx-auto min-h-screen bg-base-200 p-8 rounded-xl">
-        
+        <h2 className="text-xl mb-4 text-center text-white">Register Your Account</h2>
+      <div className="flex flex-col">
+        <input className="text-center mb-4 p-2 rounded-lg" type="text" placeholder="First Name" />
+        <input className="text-center mb-4 p-2 rounded-lg" type="text" placeholder="Last Name" />
+        <input className="text-center mb-4 p-2 rounded-lg" type="text" placeholder="Photo URL" />
+        <input className="text-center mb-4 p-2 rounded-lg" type="email" placeholder="Email" />
+        <input className="text-center mb-4 p-2 rounded-lg" type="password" placeholder="Password" />
+
+      </div>
     </div>
   );
 };
